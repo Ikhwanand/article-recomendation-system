400d5594f5d3f0e4d0318cc844b8eeb6d79e4949
ikhwanand <aldirizaldy977@gmail.com>
2025-01-01 20:58:57 +0800

    first commit

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+from pathlib import Path 
+
+# HOPSWORKS Constant
+FEATURE_GROUP_VERSION = 1
+
+# Paths
+
+PATH_SENTENCES = 'C:/article-recomendation-system/models/sentences'  # Change this path based on your local path
+PATH_EMBEDDINGS = 'C:/article-recomendation-system/models/embeddings' # Change this path based on your local path
+
+PATH_DATA_BASE = 'C:/article-recomendation-system/data' # Change this path based on your local path
+
